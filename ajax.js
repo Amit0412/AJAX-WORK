7d4e911079a049b9709593036470ace4d26a4cf9
Amit0412 <59620280+Amit0412@users.noreply.github.com>
2021-01-26 20:37:44 +0530

    AJAX GET REQUEST METHOD
    GET REQUEST VIA AJAX IN SIMPLIER LANGUAGE.

---
+++ ajax.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AJAX PRACTISE</title>
+</head>
+<body>
+    <h1>Sample ajax example</h1>
+    <button id="a" class="b">Click me</button>
+    <script>
+        //ALL ABOUT GET REQUEST
+        let r=document.getElementById("a")
+        r.addEventListener('click',clicked)
+        function clicked()
+        {
+            //Step 1 create xhr object.
+            let xhr=new XMLHttpRequest();
+
+            //Describe the request
+            xhr.open('GET',"sample.java",true)
+
+            //Try various xhr functions.
+            xhr.onprogress=function()
+            {
+                console.log("process under going")
+            }
+
+            //When response ready
+            xhr.onload=function()
+            {
+                if(this.status==200)
+                {
+                    console.log(this.responseText)
+                }
+                else
+                {
+                    console.log("some error")
+                }
+            }
+            //send the request.
+            xhr.send()
+        }
+    </script>
+</body>
+</html>
